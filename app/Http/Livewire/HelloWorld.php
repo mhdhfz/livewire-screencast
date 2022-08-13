8e07afa9fe7b89727d172c787a68c86ee11d9429
mhdhfz <mhd.hfzz@gmail.com>
2022-08-13 17:02:30 +0800

    data binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,8 +6,12 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {   
+    public $name = 'Jelly';
+    public $loud = false;
+    public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', ['name' => 'Jelly']);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,16 @@
 <div>
-    hello {{ $name }}
+    {{-- tiap kali berhenti taip dia akan hantar ajax request --}}
+    <input type="text" wire:model="name">
+
+    <input type="checkbox" wire:model="loud">
+
+    <select wire:model="greeting" multiple>
+        <option>Hello</option>
+        <option>Hai</option>
+        <option>Hey</option>
+    </select>
+
+    {{-- guna lazy kalau nak hantar ajax bila tekan butang or tekan tempat lain --}}
+    {{-- <input type="text" wire:model.lazy="name"> --}}
+    {{ implode(', ', $greeting) }} {{ $name }} @if ($loud) ! @endif
 </div>
